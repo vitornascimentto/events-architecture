996e519e12b16568abf1004844a10e3fe4fa9713
vitornascimentto <vitornascimentto0@gmail.com>
2023-04-20 15:38:59 -0300

    test(model): create user model specs

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:admin_flag) }
+  end
+end
